f8883136170737a09cdae4759db48bc3b170a1cf
sofia <rodriguezsofiaf@gmail.com>
2021-05-25 14:22:59 -0300

    Arreglar mixed content

--- components/Layout.js
+++ components/Layout.js
@@ -6,9 +6,9 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head>
-      <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-      <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-    <script src="https://unpkg.com/@google/markerclustererplus@4.0.1/dist/markerclustererplus.min.js"></script>
+        <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=false"></script>
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+        <script src="https://unpkg.com/@google/markerclustererplus@4.0.1/dist/markerclustererplus.min.js"></script>
       </Head>
 
       <Header />
